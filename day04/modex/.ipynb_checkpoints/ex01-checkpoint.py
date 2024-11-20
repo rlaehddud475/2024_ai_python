5f376d705b55dfa7b1f1211135ff136c9230d59b
TwoNewbie <hgwon4071@gmail.com>
2024-11-20 12:43:02 +0900

    20241120

---
+++ day04/modex/.ipynb_checkpoints/ex01-checkpoint.py
@@ -0,0 +1,10 @@
+import mod1
+result = mod1.add(10,20)
+print(result)
+
+
+result = mod1.sub(10,20)
+print(result)
+
+PI=mod1.PI
+print(PI)
---
+++ day04/modex/.ipynb_checkpoints/ex02-checkpoint.py
@@ -0,0 +1,4 @@
+from mod1 import add, sub, PI
+print(add(10,20))
+print(sub(20,30))
+print(PI)

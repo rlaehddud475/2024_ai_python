eb70b38f6bfc07f6fc3e0ca771a87f1f18128781
TwoNewbie <hgwon4071@gmail.com>
2024-11-21 10:42:20 +0900

    20241121

---
+++ pkg/game/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,6 @@
+from .graphic.render import render_test
+VERSION=1.0
+def show_game_version():
+    print(f"게임버젼:{VERSION}")
+
+print("----- 게임 로딩 초기화 -----")
---
+++ pkg/game/sound/__init__.py
@@ -0,0 +1,4 @@
+from ..graphic.render import render_test
+print("--- 게임 사운드 초기화 ---")
+
+__all__=['echo']
